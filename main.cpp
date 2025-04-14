6a012e4fd6029e88d2b4f949283f6d3171861c00
estoc27 <40562785+estoc27@users.noreply.github.com>
2025-04-13 20:29:55 -0400

    04/13-Aaron

---
+++ main.cpp
@@ -0,0 +1,91 @@
+// -------------------------------
+// Book Interaction (Borrow / Return / Search)
+// -------------------------------
+
+void borrowBook() {
+    // To be implemented
+}
+
+void returnBook() {
+    // To be implemented
+}
+
+void searchBook() {
+    // To be implemented (calls InfoManager)
+}
+
+void viewBookDetails() {
+    // To be implemented (calls InfoManager)
+}
+
+void checkOverdue() {
+    // To be implemented (calls InfoManager)
+}
+
+void viewBorrowingHistory() {
+    // To be implemented (calls InfoManager)
+}
+
+
+// -------------------------------
+// Authentication
+// -------------------------------
+
+void loginUser() {
+    // To be implemented
+}
+
+void logoutUser() {
+    // To be implemented
+}
+
+bool isUserLoggedIn() {
+    // To be implemented
+    return false;
+}
+
+bool isAdmin() {
+    // To be implemented
+    return false;
+}
+
+
+// -------------------------------
+// Information Management (Core Logic)
+// -------------------------------
+
+void addBook() {
+    // To be implemented
+}
+
+void editBook() {
+    // To be implemented
+}
+
+void deleteBook() {
+    // To be implemented
+}
+
+void registerUser() {
+    // To be implemented
+}
+
+void fetchBookList() {
+    // To be implemented
+}
+
+void fetchBookDetailsByID(int bookID) {
+    // To be implemented
+}
+
+void fetchUserBorrowHistory(int userID) {
+    // To be implemented
+}
+
+void fetchOverdueStatus(int userID) {
+    // To be implemented
+}
+
+
+
+
